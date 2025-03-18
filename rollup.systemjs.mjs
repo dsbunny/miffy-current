2440dc411c9f529df801e6d3f3518e4ab1ac1102
steve-o <fnjordy@gmail.com>
2025-03-18 20:03:43 +0000

    First release year is 2025. Add missing `vim` modlines for _rollup_ configurations. Update `@dsbunny/rtcmesh` to _public_ release commit tag.

--- rollup.systemjs.mjs
+++ rollup.systemjs.mjs
@@ -1,3 +1,4 @@
+// vim: tabstop=8 softtabstop=0 noexpandtab shiftwidth=8 nosmarttab
 // Copyright 2025 Digital Signage Bunny Corp. Use of this source code is
 // governed by an MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
